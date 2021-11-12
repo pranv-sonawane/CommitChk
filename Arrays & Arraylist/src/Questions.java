aaaf12bafcdad82c74f8614a7c653eb08e5727a0
Pranv <sonawanepranav19@gmail.com>
2021-11-12 16:39:03 +0530

    shuffle array

--- Arrays & Arraylist/src/Questions.java
+++ Arrays & Arraylist/src/Questions.java
@@ -1,6 +1,5 @@
 public class Questions {
     public static void main(String[] args) {
 
-        double[] arr = new double[5];
     }
 }
---
+++ Arrays & Arraylist/src/com/pranav/ShuffleArr.java
@@ -0,0 +1,26 @@
+package com.pranav;
+
+import java.util.Arrays;
+
+public class ShuffleArr {
+
+    static void shuffle(int [] a, int n){
+        int[] ans  = new int[2 * n];
+        int len = 2 * n, k = n, b = 0, c = 0 ;
+        while (b<len){
+            ans[b] = a[c];
+            b++; c++;
+            ans[b] = a[k];
+            b++; k++;
+         }
+
+        System.out.println(Arrays.toString(ans));
+    }
+
+    public static void main(String[] args) {
+        int n = 2;
+        int[] arr = {1, 4, 3, 1};
+
+        shuffle(arr, n);
+    }
+}
