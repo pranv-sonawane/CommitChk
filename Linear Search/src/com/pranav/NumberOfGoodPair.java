a36f984a9f4ec48e70549e06c8f8a8c52896c968
Pranv <sonawanepranav19@gmail.com>
2021-11-22 21:14:19 +0530

    Good Pair Leetcode Program

--- Linear Search/src/com/pranav/NumberOfGoodPair.java
+++ Linear Search/src/com/pranav/NumberOfGoodPair.java
@@ -2,9 +2,18 @@ package com.pranav;
 
 public class NumberOfGoodPair {
     public static void main(String[] args) {
-
+        int[] list = {1,1,1,1};
+        System.out.println(numIdenticalPairs(list));
     }
     static int numIdenticalPairs(int[] nums) {
-
+        int goodPair = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]){
+                    goodPair++;
+                }
+            }
+        }
+        return goodPair;
     }
 }
