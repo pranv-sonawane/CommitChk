23244522354f917d9d9222d468f590621b1dce21
Pranv <sonawanepranav19@gmail.com>
2021-11-22 21:16:03 +0530

    Number smaller than current leetcode

--- Linear Search/src/com/pranav/NumsSmallerThanCurrNum.java
+++ Linear Search/src/com/pranav/NumsSmallerThanCurrNum.java
@@ -1,2 +1,28 @@
-package com.pranav;public class NumsSmallerThanCurrNum {
+package com.pranav;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class NumsSmallerThanCurrNum {
+    public static void main(String[] args) {
+        int[] nums = {6,5,4,8};
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(nums)));
+    }
+    static int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] ans = new int[nums.length];
+        int counter;
+        for (int i = 0; i < nums.length; i++) {
+            counter = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] == nums[i]){
+                    continue;
+                }
+                if (nums[i] > nums[j]){
+                    counter++;
+                }
+            }
+            ans[i] = counter;
+        }
+        return ans;
+    }
 }
